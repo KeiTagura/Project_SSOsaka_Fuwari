e3d9a87179d1793da450024d104e179da32958bf
KeiTagura <34691764+KeiTagura@users.noreply.github.com>
2025-06-19 06:47:11 +0900

    events page

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -15,4 +15,9 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 		name: i18n(I18nKey.archive),
 		url: "/archive/",
 	},
+	//
+	[LinkPreset.Events]: {
+		name: i18n(I18nKey.events),
+		url: "/events/",
+	},
 };
--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -6,6 +6,7 @@ export const en: Translation = {
 	[Key.about]: "About",
 	[Key.archive]: "Archive",
 	[Key.search]: "Search",
+	[Key.events]: "Events",
 
 	[Key.tags]: "Tags",
 	[Key.categories]: "Categories",
