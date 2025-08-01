b1e554871971ff78cde605d98a1d95c92ffdd3cd
KeiTagura <34691764+KeiTagura@users.noreply.github.com>
2025-08-02 00:04:12 +0900

    banner mod

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,11 +3,11 @@ export const PAGE_SIZE = 8;
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
 	AUTO_MODE = "auto";
-export const DEFAULT_THEME = AUTO_MODE;
+export const DEFAULT_THEME = DARK_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 100;
-export const BANNER_HEIGHT_EXTEND = 0;
+export const BANNER_HEIGHT = 10;
+export const BANNER_HEIGHT_EXTEND = 90;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
 // The height the main panel overlaps the banner, unit: rem
