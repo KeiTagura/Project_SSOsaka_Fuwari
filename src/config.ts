6de4acd6bec76214533508c0b5c898d6f0498525
KeiTagura <34691764+KeiTagura@users.noreply.github.com>
2025-10-21 01:58:22 +0900

    language select persistance

--- src/config.ts
+++ src/config.ts
@@ -46,11 +46,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		//LinkPreset.Events,
-		{
+		/*{
 			name: "GitHub",
 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
-		},
+		},*/
 	],
 };
 
